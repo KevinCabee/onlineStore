eacc9d3326c8da9e5d13b8d99c18462c6cabe7bc
KevinCabee <kcabrera913@gmail.com>
2024-05-20 19:53:59 -0400

    assigment 4

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+from config import db
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 def home():
     return "Hello from flask"
 
-# Create another API that redirects you to a test
+# Create another App Programming Interface (API) that redirects you to a test
 @app.route("/test")
 def test():
     return "This is a test page"
@@ -22,5 +23,22 @@ def server():
 def about():
     myname={"name": "kevin cabrera"}
     return json.dumps(myname)
+
+products = []
+def fix_id(obj):
+    obj["_id"] = str(obj["_id"])
+    return obj
+@app.post("/api/products")
+def save_product():
+    newItem=request.get_json()
+    print(newItem)
+    # products.append(newItem)
+    db.products.insert_one(newItem)
+    return json.dumps(fix_id(newItem))
+
+@app.get("/api/products")
+def get_product():
+    return json.dumps(products)
+
 
 app.run(debug=True)
